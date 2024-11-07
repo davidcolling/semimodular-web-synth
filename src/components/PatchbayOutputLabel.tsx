b17ccfa47ef06e6136c107b0e794b78cb5bf873e
David Colling <d@dcolling.com>
2024-11-07 20:45:26 +0100

    Adjust styles in PatchbayOutputLabel

--- src/components/PatchbayOutputLabel.tsx
+++ src/components/PatchbayOutputLabel.tsx
@@ -4,14 +4,16 @@ import { PatchbayInputLabelProps } from "../types";
 const PatchbayOutputLabel = ({ name }: PatchbayInputLabelProps ) => {
   return (
     <div>
-      <div className="rotated">
+      <div className="title-small rotated">
         <p>{name}</p>
       </div>
       <svg
         width={10}
         height={10}
         style={{
-          display: "inline",
+          display: "block",
+          margin: "auto",
+          paddingTop: "10px"
         }}
       >
          <polygon points="5 0, 0 10, 10 10" />
